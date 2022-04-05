d5b37eaf269dd5eb62412cef3f5cff5e48b1f802
Omkar Savkur <osavkur@andrew.cmu.edu>
2022-04-05 13:52:12 -0400

    Added automatic map generation from image

---
+++ maps/generateMap.py
@@ -0,0 +1,29 @@
+# generateMap.py
+import sys
+from PIL import Image
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python3 generateMap.py {image_file_name} {output_file_name}")
+    
+    imageFileName = sys.argv[1]
+    mapFileName = sys.argv[2]
+
+    im = Image.open(imageFileName)
+    width, height = im.size
+
+    output = open(mapFileName, "w")
+
+    for y in range(height):
+        for x in range(width):
+            coordinate = x,y
+            px = im.getpixel(coordinate)
+            if (px > 0):
+                output.write('1')
+            else:
+                output.write('0')
+        output.write("\n")
+    output.write("\n")
+    
+    print("Done writing to file")
+
