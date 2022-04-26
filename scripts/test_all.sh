23f2656e435985104d235966c4f327716dde120b
Prithu Pareek <ppareek@br013.ib.bridges2.psc.edu>
2022-04-26 12:17:06 -0400

    First round of tests + updated bash

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -4,10 +4,11 @@ echo "Testing all maps"
 echo "================"
 
 # convert all test_scripts files to executable
-chmod +x test_scripts/*.sh
+chmod +x ~/15418-Final-Project/scripts/test_scripts/*.sh
 
 # run all sh files in the test_scripts directory
-for file in test_scripts/*.sh
-    echo "Running $file"
-    ./$file
+for file in ~/15418-Final-Project/scripts/test_scripts/*.sh
+do
+	echo "Testing $file"
+	$file
 done
